b523a2ed73b85594fbfde068bd6a3dc406bb3ea6
Eclaire31 <eclaire31.3d@gmail.com>
2025-12-03 16:33:26 +0100

    Modify NoNav.bat to terminate chrome.exe instead of explorer.exe

--- NoNav.bat
+++ NoNav.bat
@@ -1,9 +1,9 @@
 @echo off
 msg * Pour couper le NoNav ouvre la fenêtre du cmd et fait CRLT+C
 :home
-tasklist | find "explorer.exe" >nul
+tasklist | find "chrome.exe" >nul
 if %errorlevel% equ 0 (
-    taskkill /f /im explorer.exe >nul
+    taskkill /f /im chrome.exe >nul
 )
 
 goto home
